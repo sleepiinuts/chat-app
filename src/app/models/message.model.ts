65800daea1db8c34b4ad2800844bcef7f66a1803
sleepynut <nut.pa@kbtg.tech>
2025-01-07 17:39:49 +0700

    add underfined user for initial state

--- src/app/models/message.model.ts
+++ src/app/models/message.model.ts
@@ -3,7 +3,7 @@ import { User } from './user.model';
 export class Message {
   constructor(
     public id: string,
-    public user: User,
+    public user: User | undefined,
     public timestamp: Date,
     public text: string
   ) {}
