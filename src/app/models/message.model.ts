cc54c552ca90a8f645beeb4a53f0d75774952d43
sleepynut <nut.pa@kbtg.tech>
2025-01-07 12:44:08 +0700

    user & message classes

--- src/app/models/message.model.ts
+++ src/app/models/message.model.ts
@@ -1,8 +1,10 @@
 import { User } from './user.model';
 
-export interface Message {
-  id: string;
-  user: User;
-  timestamp: Date;
-  text: string;
+export class Message {
+  constructor(
+    public id: string,
+    public user: User,
+    public timestamp: Date,
+    public text: string
+  ) {}
 }
