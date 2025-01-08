dea09efecba4abe1d2a87e80e2518f123ed64dee
sleepynut <nut.pa@kbtg.tech>
2025-01-08 14:28:30 +0700

    dynamic chatThread

--- src/app/components/chat-threads/chat-thread/chat-thread.component.ts
+++ src/app/components/chat-threads/chat-thread/chat-thread.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Thread } from '../../../models/thread.model';
+import { ChatThreadsActions } from '../../../ngrx-store/chat-threads/chat-threads.actions';
 
 @Component({
   selector: 'app-chat-thread',
   imports: [],
   templateUrl: './chat-thread.component.html',
-  styleUrl: './chat-thread.component.scss'
+  styleUrl: './chat-thread.component.scss',
 })
 export class ChatThreadComponent {
+  thread = input.required<Thread>();
 
+  constructor(private store: Store) {}
+
+  setCurrentThread() {
+    this.store.dispatch(
+      ChatThreadsActions.setCurrentThread({
+        currentThread: this.thread().bot.id,
+      })
+    );
+  }
 }
--- src/app/components/chat-threads/chat-threads.component.ts
+++ src/app/components/chat-threads/chat-threads.component.ts
@@ -1,10 +1,26 @@
+import { KeyValuePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Thread } from '../../models/thread.model';
+import { User } from '../../models/user.model';
+import { BotsService } from '../bots/bots.service';
 import { ChatThreadComponent } from './chat-thread/chat-thread.component';
 
 @Component({
   selector: 'app-chat-threads',
-  imports: [ChatThreadComponent],
+  imports: [ChatThreadComponent, KeyValuePipe],
   templateUrl: './chat-threads.component.html',
   styleUrl: './chat-threads.component.scss',
 })
-export class ChatThreadsComponent {}
+export class ChatThreadsComponent {
+  users: User[];
+  threads = new Map<string, Thread>();
+
+  constructor(private botServ: BotsService) {
+    this.users = this.botServ.getBots();
+
+    // initial threads map; 1 thread: 1 bot
+    this.users.forEach((u) => {
+      this.threads.set(u.id, new Thread(u, undefined, undefined));
+    });
+  }
+}
