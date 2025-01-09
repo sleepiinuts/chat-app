60a537d8925095b97fd12add26446b7a904d95d6
sleepynut <nut.pa@kbtg.tech>
2025-01-09 16:49:21 +0700

    set intial state for chatThreads

--- src/app/ngrx-store/chat-threads/chat-threads.reducer.ts
+++ src/app/ngrx-store/chat-threads/chat-threads.reducer.ts
@@ -1,21 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Action, ActionReducer, createReducer, on } from '@ngrx/store';
 import { BotsService } from '../../components/bots/bots.service';
+import { Thread } from '../../models/thread.model';
 import { ChatThreadsActions } from './chat-threads.actions';
 
 export const chatThreadsFeatureKey = 'chatThreads';
 
 export interface State {
   currentThread: string;
+  threads: Record<string, Thread>; // equivalent to map<string,Thread> js; but works with angular devtool;
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChatThreadsReducer {
-  initialState: State;
+  initialState: State = {
+    currentThread: '1',
+    threads: {},
+  };
   constructor(private botsServ: BotsService) {
-    this.initialState = { currentThread: '' };
+    // do the actual initial state setup
+    this.initialThreads();
+  }
+
+  initialThreads() {
+    const bots = this.botsServ.getBots();
+
+    // set up thread for each bot
+    bots.forEach((b) => {
+      this.initialState.threads[b.id] = new Thread(b, undefined, undefined);
+    });
   }
 
   createReducer(): ActionReducer<State, Action> {
