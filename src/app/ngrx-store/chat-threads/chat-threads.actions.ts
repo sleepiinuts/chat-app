28ef7b7af02bad4139fad0a4a7f93c87b8beb08d
sleepynut <nut.pa@kbtg.tech>
2025-01-10 14:43:07 +0700

    switchThread action

--- src/app/ngrx-store/chat-threads/chat-threads.actions.ts
+++ src/app/ngrx-store/chat-threads/chat-threads.actions.ts
@@ -6,5 +6,6 @@ export const ChatThreadsActions = createActionGroup({
   events: {
     'Set CurrentThread': props<{ currentThread: string }>(),
     'New Messages': props<{ threadId: string; messages: Message[] }>(),
+    'Switch Thread': props<{ threadId: string }>(),
   },
 });
