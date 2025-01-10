da3d973331bd7deb5288bd76259137bf96c3b7db
sleepynut <nut.pa@kbtg.tech>
2025-01-10 14:47:09 +0700

    separate each thread messages and make chatwindow display only messages in current thread

--- src/app/components/chat-window/chat-window.component.ts
+++ src/app/components/chat-window/chat-window.component.ts
@@ -3,12 +3,14 @@ import {
   AfterViewInit,
   Component,
   computed,
+  DestroyRef,
   effect,
   Injector,
   Signal,
   ViewChild,
   ViewContainerRef,
 } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import {
   FormControl,
   FormGroup,
@@ -16,7 +18,9 @@ import {
   Validators,
 } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { Actions, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
+import { take } from 'rxjs';
 import { v4 as uuidv4 } from 'uuid';
 import { Message } from '../../models/message.model';
 import { User } from '../../models/user.model';
@@ -24,7 +28,9 @@ import {
   selectCurrentThreadId,
   selectPrompt,
   selectResponse,
+  selectThreadMessages,
 } from '../../ngrx-store/all.selector';
+import { ChatThreadsActions } from '../../ngrx-store/chat-threads/chat-threads.actions';
 import { ChatWindowActions } from '../../ngrx-store/chat-window/chat-window.actions';
 import { BotsService } from '../bots/bots.service';
 import { MessageComponent } from '../message/message.component';
@@ -54,29 +60,36 @@ export class ChatWindowComponent implements AfterViewInit {
     return this.getBotName(botId);
   });
 
+  // viewcontainerRef
   @ViewChild('vcr', { read: ViewContainerRef }) _msgVcr!: ViewContainerRef;
 
   constructor(
     private store: Store,
     private injector: Injector,
-    private botServ: BotsService
+    private botServ: BotsService,
+    private action$: Actions,
+    private destroyRef: DestroyRef
   ) {
     this.#promptMessage = this.store.selectSignal(selectPrompt);
     this.#responseMessage = this.store.selectSignal(selectResponse);
+
+    this.action$
+      .pipe(ofType(ChatThreadsActions.setCurrentThread))
+      .subscribe(() => {
+        this.onThreadChange();
+      });
   }
 
   ngAfterViewInit(): void {
     effect(
       () => {
-        // console.log(`promp signal: ${this.#promptMessage()?.text}`);
-        // console.log(`response signal: ${this.#responseMessage()?.text}`);
         this.displayMessage(this.#promptMessage());
         this.displayMessage(this.#responseMessage());
+
+        // this.onThreadChange(this.#currentThread());
       },
       { injector: this.injector }
     );
-    // this._msgVcr.createComponent(MessageComponent);
-    // this._msgVcr.createComponent(MessageComponent);
   }
 
   onSubmit() {
@@ -91,6 +104,19 @@ export class ChatWindowComponent implements AfterViewInit {
 
     // reset form
     this.textForm.reset();
+  }
+
+  onThreadChange() {
+    // clear viewcontainerRef
+    this._msgVcr.clear();
+
+    // rerender each message from current thread
+    this.store
+      .select(selectThreadMessages)
+      .pipe(take(1), takeUntilDestroyed(this.destroyRef))
+      .subscribe((messages) => {
+        messages?.forEach((m) => this.displayMessage(m));
+      });
   }
 
   displayMessage(msg: Message | undefined) {
