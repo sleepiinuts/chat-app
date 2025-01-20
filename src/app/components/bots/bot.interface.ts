6ce1f5a828a76bf336be1221859ab4d0a8ccc2f8
sleepynut <nut.pa@kbtg.tech>
2025-01-20 15:41:56 +0700

    make reply from Bot interface return Observable<Message>

--- src/app/components/bots/bot.interface.ts
+++ src/app/components/bots/bot.interface.ts
@@ -1,7 +1,8 @@
 import { Observable } from 'rxjs';
+import { Message } from '../../models/message.model';
 import { User } from '../../models/user.model';
 
 export interface Bot {
   readonly user: User;
-  reply: (msg: string) => Observable<string>;
+  reply: (msg: string) => Observable<Message>;
 }
